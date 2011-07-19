f610484600f646b9c4d2deb7ed0ff9be23b46e94
mat2uken <mat2uken@localhost>
2011-07-19 22:48:30 +0900

    nickname?User???????????????

--- applogging.py
+++ applogging.py
@@ -12,19 +12,18 @@ Message:
 """
 
 FILE_LOG_FORMAT = """%(levelname)s %(asctime)s %(pathname)s:%(lineno)s >>> %(message)s"""
+
+
 from flask import request
-def ignore_url():
-    from flask import request
-    requrl = request.url
-    if 'favicon' in requrl or 'imgsrc' in requrl or 'healthcheck' in requrl:
-        return True
-    else:
-        return False
+def ignore_url(ignore_urls):
+    rurl = request.url
+    for s in ignore_urls:
+        if s in rurl: return True
+    return False
+
 import sys
 def initlogging(app):
     import logging, logging.handlers
-
-    app.logger.setLevel(app.config.get('LOGGING_LEVEL', logging.DEBUG))
 
     # DEBUG flag is not True set several handler for production.
     if not app.config.get('DEBUG', True):
@@ -34,7 +33,7 @@ def initlogging(app):
         hostname = socket.gethostname()
         exc_mailto = app.config.get('EXCEPTION_MAILTO')
         if exc_mailto is not None:
-            mh = logging.handlers.SMTPHandler(app.config['MAIL_SERVER'], 'server-error@filejet.jp', exc_mailto, 
+            mh = logging.handlers.SMTPHandler(app.config['MAIL_SERVER'], 'info@cerevo.com', exc_mailto, 
                                               'FileJet Application Failed on %s' % hostname)
 
             mh.setFormatter(logging.Formatter(SMTP_LOG_FORMAT))
@@ -52,35 +51,43 @@ def initlogging(app):
         REQUEST_END_LOGGING_FORMAT    = "[  REQEND][user=%s] %04s %s %s"
 
         current_user = app.current_user
+        ignore_urls = app.config.get('LOGGING_IGNORE_URL')
 
         @app.before_request
         def before_request_logging():
-            if ignore_url(): return
+            if ignore_urls and ignore_url(ignore_urls): return
 
             try:
                 app.logger.debug(REQUEST_START_LOGGING_FORMAT % (
-                                 "%05s,%10s" % (current_user.id, current_user.nickname.encode('utf-8', errors="ignore"))[:10] if current_user.is_authenticated() else "anonymous",
+                                 "%05s,%10s" % (current_user.id, repr(current_user).encode('utf-8', errors="ignore"))[:10] if current_user.is_authenticated() else "anonymous",
                                  request.method, request.path))
             except:
-                pass
+                import traceback
+                traceback.print_exc(file=sys.stderr)
 
         @app.after_request
         def after_request_logging(response):
-            if ignore_url(): return response
+            if ignore_urls and ignore_url(ignore_urls): return response
 
             try:
                 if response.status_code < 400:
                     app.logger.debug(REQUEST_END_LOGGING_FORMAT % (
-                                     "%05s,%10s" % (current_user.id, current_user.nickname.encode('utf-8', errors="ignore"))[:10] if current_user.is_authenticated() else "anonymous",
+                                     "%05s,%10s" % (current_user.id, repr(current_user).encode('utf-8', errors="ignore"))[:10] if current_user.is_authenticated() else "anonymous",
                                      request.method, request.path, response.status_code))
                 else:
                     app.logger.info(REQUEST_END_LOGGING_FORMAT % (
-                                    "%05s,%10s" % (current_user.id, current_user.nickname.encode('utf-8', errors="ignore"))[:10] if current_user.is_authenticated() else "anonymous",
+                                    "%05s,%10s" % (current_user.id, repr(current_user).encode('utf-8', errors="ignore"))[:10] if current_user.is_authenticated() else "anonymous",
                                     request.method, request.path, response.status_code))
             except:
-                pass
+                import traceback
+                traceback.print_exc(file=sys.stderr)
 
             return response
+
+    print >>sys.stderr, "app.config['DEBUG'] => %s" % app.config.get('DEBUG')
+    print >>sys.stderr, "app.config['LOGGING_LEVEL'] => %s" % app.config.get('LOGGING_LEVEL')
+
+    app.logger.setLevel(app.config.get('LOGGING_LEVEL', logging.DEBUG))
 
     if app.config.get('DEBUG', False):
         app.logger.debug('logging startup test: level=>DEBUG')

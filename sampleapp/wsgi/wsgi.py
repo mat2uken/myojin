ae9cd758ab66b057807d950eb470253161974299
mat2uken <62655+mat2uken@users.noreply.github.com>
2011-07-19 18:20:39 +0900

    wsgi???????????

--- sampleapp/wsgi/wsgi.py
+++ sampleapp/wsgi/wsgi.py
@@ -1,21 +1,28 @@
 import os
 import sys
 
+app = None
+INSTALLED = False
+
 def application(environ, start_response):
+    global INSTALLED
+    global app
+    if INSTALLED and app: return app(environ, start_response)
+
     # activate virtualenv
     activate_this = environ['VIRTUALENV_ACTIVATE_THIS']
     execfile(activate_this, dict(__file__=activate_this))
-
-    ## import your app
-    from sampleapp import app
+    
+    from sampleapp import app as sample_app
+    app = sample_app
 
     from myojin import custom_script
     config_name = environ['MYOJIN_CONFIG_NAME']
     print >>sys.stderr, '*' * 20, "initializing wsgi application by %s.cfg" % config_name, '*' * 20
-    custom_script.config_from_file(defaults=('dev.cfg', '%s.cfg' % config_name,), app=app)
+    custom_script.config_from_file(config="%s.cfg" % config_name, defaults=('wsgi.cfg',),
+                                   app=app, use_username_config=False)
 
-    from myojin.applogging import initlogging
-    initlogging(app)
-
-    app.logger.info('start wsgi application!')    
+    INSTALLED = True
+    app.logger.info('initialized wsgi application!')
     return app(environ, start_response)
+

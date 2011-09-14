cc58a972f1a332268e294c63c886b729a81b3379
hokari <hokari@localhost>
2011-09-14 20:33:27 +0900

    torii branch ?? sample?myapp/apps/main/__init__.py?submodule?????

--- sampleapp/myapp/apps/main/__init__.py
+++ sampleapp/myapp/apps/main/__init__.py
@@ -2,4 +2,7 @@ from myojin.submodule import Module
 
 module = Module(__name__, url_prefix="")
 from . import views
-views.top.module.register_to(module)
+for name, submodule in views.__dict__.items():
+    if not name.startswith("_") and hasattr(submodule,"module"):
+        submodule.module.register_to(module)
+## views.top.module.register_to(module)

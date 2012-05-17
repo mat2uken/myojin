1a0eb04d30d97a9865e470c9c03a24a0d96960cb
mat2uken <62655+mat2uken@users.noreply.github.com>
2012-05-17 12:22:43 +0000

    mako?static()???

--- mako.py
+++ mako.py
@@ -11,6 +11,17 @@ def init(app,globals=None):
     _globals = globals or dict()
     _globals['DEBUG'] = app.config.get('DEBUG', False)
     _globals['url_for'] = url_for
+
+    import os
+    from flask import request
+    static_url_path = app.config.get('STATIC_URL_PATH', '/static')
+    def static(p):
+        abs_url_path = os.path.join(static_url_path, p)
+        if request.is_secure:
+            return abs_url_path.replace('http', 'https')
+        else:
+            return abs_url_path.replace('https', 'http')
+    _globals['static'] = static
 _globals = None
 
 def get_template(template_name):
@@ -21,19 +32,10 @@ def init_lookup(directory, module_directory, globals=None):
     return TemplateLookup(directories=[directory], module_directory=module_directory,
                           input_encoding="utf-8",
                           output_encoding="utf-8",
-                          #imports=["from json import dumps as JSON"],
                           default_filters=['h'])
     
-##     global _lookup
-##     global _globals
-##     _globals = globals or dict()
-##     _lookup = TemplateLookup(directories=[directory], module_directory=module_directory,input_encoding="utf-8")
-
 def render(template_name, ctx=dict(), to_unicode=False):#, *args, **kws):
     tmpl = get_template(template_name)
-##     from flask import _request_ctx_stack
-##     app = _request_ctx_stack.top.app
-##     lookup2 = init_lookup(app.root_path + "/templates", app.root_path + "/tmp", globals=globals)
     
     # TODO: cache tmpl
     render = tmpl.render if to_unicode else tmpl.render_unicode
@@ -44,8 +46,6 @@ def render(template_name, ctx=dict(), to_unicode=False):#, *args, **kws):
 from mako.filters import html_escape
 from markupsafe import escape, Markup
 def newline_filter(s, escape=False):
-    #assert isinstance(s, basestring)
-    #assert isinstance(s, unicode)
     if isinstance(s, str):
         s = unicode(s)
     elif isinstance(s, unicode):
@@ -60,10 +60,11 @@ from json import dumps
 def json_dumps(s):
     assert isinstance(s, (list,dict))
     return Markup(dumps(s))
-    #dumps
+
 def debug_space(s):
     assert isinstance(s, unicode)
     return unicode(s).replace(u' ',u'　')
+
 try:
     from flaskext.babel import gettext # as _
 except ImportError, e:
@@ -81,7 +82,7 @@ def get_locale():
         return babel.get_locale()
     except:
         return None
-        
+
 mako_utils = dict(
     _=gettext,
     newline=newline_filter,
